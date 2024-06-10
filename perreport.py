1b777f500cfbe24d0e032cfeaff149301ad2b45e
syoncodes <34947109+syoncodes@users.noreply.github.com>
2024-06-10 16:39:47 -0500

    Update perreport.py

--- perreport.py
+++ perreport.py
@@ -11,7 +11,7 @@ client = OpenAI(
 # MongoDB Atlas connection
 mongo_client = pymongo.MongoClient('mongodb://syonb:syonsmart@ac-0w6souu-shard-00-00.jfanqj5.mongodb.net:27017,ac-0w6souu-shard-00-01.jfanqj5.mongodb.net:27017,ac-0w6souu-shard-00-02.jfanqj5.mongodb.net:27017/?replicaSet=atlas-yytbi1-shard-0&ssl=true&authSource=admin')
 db = mongo_client['test']
-collection = db['organizations']
+collection = db['users']
 
 # Function to format data for the vulnerability report
 def format_report_data(doc):
